ff092b63bf7eb66f00c19f45438caeff7dc69daa
YuriKalani <yuri.kalani@hotmail.com>
2021-01-15 01:09:52 -0300

    ex011 - Teste de conhecimento

---
+++ Aula 11/ex008 e 009.js
@@ -0,0 +1,8 @@
+var vel = 50
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você ULTRAPASSOU a velocidade! MULTADO!!')
+} else {
+    console.log("Você está na velocidade adequada! Boa viagem!")
+}
+console.log("Dirija sempre com o cinto")
