ce42d4cb77f2ad662578d06b0d0b44be39b73ed3
YuriKalani <yuri.kalani@hotmail.com>
2021-01-24 01:23:05 -0300

    Commit 24/01 - Treino

--- Aula 12 ex/ex015/JScript.js
+++ Aula 12 ex/ex015/JScript.js
@@ -1,13 +1,49 @@
-function verificacao() {
-    var data = new Date ()
-    var ano = data.getFullYear()
-    var fAno = window.document.getElementById('txtano')
-    var res = window.document.querySelector('div#res')
-    if (fAno.nodeValue.length == 0 || Number(fAno.value) > ano) {
+function verificar() {
+    let data = new Date ()
+    let ano = data.getFullYear()
+    let fAno = window.document.getElementById('txtano')
+    let res = window.document.querySelector("div#res")
+    if (fAno.value.length == 0 || Number(fAno.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var fsex = window.document.getElementsByName('radsex')
         var idade = ano - Number(fAno.value)
-        res.innerHTML = `Idade calculada ${idade}`
+        let gênero = " "
+        let img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'babyman.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'teenman.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'adultman.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'oldman.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'babywoman.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'teenwoman.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'adultwoman.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'oldwoman.png')
+            }
+        }
+        res.style.textAlign = "Center"
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
