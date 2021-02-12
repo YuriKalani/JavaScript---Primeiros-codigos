7764dc1e4db7a21f6dad2855000987b033f46881
YuriKalani <yuri.kalani@hotmail.com>
2021-02-12 16:11:13 -0300

    Do - While (Enquanto)

---
+++ Aula 13/Enquanto - while.js
@@ -0,0 +1,18 @@
+var c = 1
+while (c <= 500) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
