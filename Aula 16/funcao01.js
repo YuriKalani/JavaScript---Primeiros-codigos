d93c1bf5670affceb34295dec21bcb9169372cf3
YuriKalani <yuri.kalani@hotmail.com>
2021-02-24 15:27:41 -0300

    commit

---
+++ Aula 16/funcao01.js
@@ -0,0 +1,12 @@
+function parimp (num) {
+    if (num%2==0) {
+        return 'PAR'
+    } else {
+        return 'IMPAR'
+    }
+}
+
+/*let res = parimp(8)
+console.log(res)*/
+
+console.log(parimp(223))
